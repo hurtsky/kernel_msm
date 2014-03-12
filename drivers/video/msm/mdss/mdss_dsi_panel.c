bb5c245e384ccd2ed9c7ed98d7b99ebc860207d9
Jared Suttles <jsuttles@motorola.com>
2014-03-12 11:07:13 -0500

    msm: mdss: add pre_display_on and display_off notifications
    These notifications are required to allow for work to be done immediately
    before the panel is turned on and immediately after the panel is turned off
    
    Change-Id: I8fd93d3195efd5f1d01c6bd99734ed6e45628b0c
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/623837
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -527,6 +527,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	pr_debug("%s+: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
+	mmi_panel_notify(MMI_PANEL_EVENT_PRE_DISPLAY_ON, NULL);
+
 	mdss_dsi_panel_regulator_on(pdata, 1);
 
 	if (ctrl->on_cmds.cmd_cnt)
@@ -566,11 +568,14 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	mdss_dsi_panel_reset(pdata, 0);
 	mdss_dsi_panel_regulator_on(pdata, 0);
 
+	mmi_panel_notify(MMI_PANEL_EVENT_DISPLAY_OFF, NULL);
+
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 
 	pr_info("%s:-\n", __func__);
+
 	return 0;
 }
 
