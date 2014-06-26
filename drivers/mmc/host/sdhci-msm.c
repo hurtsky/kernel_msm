f301fb24e292d7afa0c2060eaa3bf68c7a42e881
Russ W. Knize <Russ.Knize@motorola.com>
2014-06-26 11:09:36 -0500

    sdhci-msm: skip SDIO and SD probing for eMMCs
    If a host is attached to a non-removable card, assume it is an eMMC and
    avoid probing for other card types.  This saves >20ms on each probe.
    
    Change-Id: I8a9bcc757adf52cef4e645fb2805a4bd4884b569
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/651944
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    
    Conflicts:
    	drivers/mmc/host/sdhci-msm.c

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -294,6 +294,7 @@ struct sdhci_msm_pltfm_data {
 	unsigned long mmc_bus_width;
 	struct sdhci_msm_slot_reg_data *vreg_data;
 	bool nonremovable;
+	bool is_emmc;
 	struct sdhci_msm_pin_data *pin_data;
 	u8 drv_types;
 	u32 cpu_dma_latency_us;
@@ -1504,6 +1505,9 @@ static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev)
 		pdata->mpm_sdiowakeup_int = mpm_int;
 	else
 		pdata->mpm_sdiowakeup_int = -1;
+
+	if (of_get_property(np, "qcom,emmc", NULL))
+		pdata->is_emmc = true;
 
 	return pdata;
 out:
@@ -3154,6 +3158,9 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 
 	if (msm_host->pdata->nonremovable)
 		msm_host->mmc->caps |= MMC_CAP_NONREMOVABLE;
+
+	if (msm_host->pdata->is_emmc)
+		msm_host->mmc->caps2 |= MMC_CAP2_MMC_ONLY;
 
 	if (mmc_host_uhs(msm_host->mmc)) {
 		sdhci_caps = readl_relaxed(host->ioaddr + SDHCI_CAPABILITIES_1);
