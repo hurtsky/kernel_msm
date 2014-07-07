433fd2045740bc1804bf106dfc4c1b60e18375a0
Russ W. Knize <Russ.Knize@motorola.com>
2014-07-07 09:52:27 -0500

    mmc: cleanup raw command logging
    The raw command logger has suffered some bit rot after being merged
    a few times.  This commit cleans that up, hooks it into sdhci, fixes
    some usability bugs, reduces the performance impact when disabled, and
    makes it a bit easier to configure from internal test code.
    
    Change-Id: I1ca7366734af0f4a91d80470ff0ecc75b006c5aa
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/651201
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1165,6 +1165,8 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 
 	if (cmd->data)
 		host->data_start_time = ktime_get();
+
+	mmc_cmd_log(host->mmc, cmd->opcode, cmd->arg);
 	sdhci_writew(host, SDHCI_MAKE_CMD(cmd->opcode, flags), SDHCI_COMMAND);
 }
 
@@ -1188,6 +1190,7 @@ static void sdhci_finish_command(struct sdhci_host *host)
 		} else {
 			host->cmd->resp[0] = sdhci_readl(host, SDHCI_RESPONSE);
 		}
+		mmc_cmd_log_resp(host->mmc, host->cmd->resp[0]);
 	}
 
 	host->cmd->error = 0;
